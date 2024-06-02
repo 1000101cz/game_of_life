814f4464512c6bf306774dbcdbdce1aa5fa64d9c
Štěpán Marousek <71834145+1000101cz@users.noreply.github.com>
2024-06-02 14:22:48 +0200

    Update game_of_life.py

--- game_of_life.py
+++ game_of_life.py
@@ -1,13 +1,21 @@
+import os
 import math
-import pathlib as pl
 import numpy as np
+import pathlib as pl
 from time import sleep
 import pyqtgraph as pg
 from loguru import logger
-from PyQt5 import uic, QtWidgets, QtCore, QtGui
-from PyQt5.QtCore import pyqtSignal, QObject, QThread
-from PyQt5.QtWidgets import QWidget, QMainWindow
+from PyQt5 import QtCore, QtGui, uic
+from PyQt5.QtCore import pyqtSignal, QObject, QThread, Qt
+from PyQt5.QtWidgets import QWidget, QDialog
 
+
+plugin_name = "MainGameOfLife"
+button_name = "Game of Life"
+no_data_enabled = True
+
+_help_path = pl.Path(__file__).parent / 'src' / 'help_dialog.ui'
+_help_dialog = uic.loadUiType(_help_path)[0]
 
 pg.setConfigOption('foreground', 'k')
 pg.setConfigOption('background', (255, 255, 255, 0))
@@ -16,6 +24,60 @@ _grid_10x10 = '10x10'
 _grid_25x25 = '25x25'
 _grid_50x50 = '50x50'
 _grid_custom = 'Custom'
+
+
+def load_preset(preset_name: str) -> np.ndarray:
+    if not preset_name.endswith('.npy'):
+        preset_name += '.npy'
+    fpath = pl.Path(__file__).parent / 'src' / 'presets' / preset_name
+    assert fpath.is_file()
+    return np.load(fpath)
+
+
+def remove_preset(preset_name: str) -> bool:
+    if not preset_name.endswith('.npy'):
+        preset_name += '.npy'
+    try:
+        fpath = pl.Path(__file__).parent / 'src' / 'presets' / preset_name
+        os.remove(fpath)
+        return True
+    except Exception as e:
+        logger.error(f"Cannot remove preset {preset_name}")
+        logger.debug(e)
+        return False
+
+
+def save_preset(preset_name: str, preset: np.ndarray) -> None:
+    if not preset_name.endswith('.npy'):
+        preset_name += '.npy'
+    fpath = pl.Path(__file__).parent / 'src' / 'presets' / preset_name
+    np.save(fpath, preset)
+
+
+def check_preset_name(preset_name: str) -> bool:
+    if not preset_name.endswith('.npy'):
+        preset_name += '.npy'
+    if not len(preset_name[:-4]):
+        return False
+    dot_found = False
+    for char in preset_name:
+        if char.lower() not in 'qwertyuioplkjhgfdsazxcvbnm._0123456789':
+            return False
+        if char == '.':
+            if dot_found:
+                return False
+            dot_found = True
+    if preset_name[:-4] in presets_list():
+        return False
+    return True
+
+
+def presets_list() -> list[str]:
+    path = pl.Path(__file__).parent / 'src' / 'presets'
+    a = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+    for i in range(len(a)):
+        a[i] = a[i][:-4]
+    return a
 
 
 class _RectItem(pg.GraphicsObject):
@@ -55,7 +117,8 @@ class GameGridPlot:
         self.plot_graph.hideButtons()
         widget.addWidget(self.plot_graph)
         self.size = None
-        self.grid = []
+        self.grid_objects = []
+        self.grid_visible = []
 
         self.plot_graph.scene().sigMouseClicked.connect(self._mouse_clicked)
 
@@ -64,30 +127,34 @@ class GameGridPlot:
         self.plot_graph.clear()
         pen = pg.mkPen('k', width=2)
         for i in range(-1, size[0]):
-            self.plot_graph.plot([i+1, i+1], [0, size[1]], pen=pen)
+            self.plot_graph.plot([i + 1, i + 1], [0, size[1]], pen=pen)
         for i in range(-1, size[1]):
-            self.plot_graph.plot([0, size[0]], [i+1, i+1], pen=pen)
-        self.grid = []
+            self.plot_graph.plot([0, size[0]], [i + 1, i + 1], pen=pen)
+        self.grid_visible = []
+        self.grid_objects = []
         for i in range(self.size[0]):
-            self.grid.append([])
+            self.grid_visible.append([])
+            self.grid_objects.append([])
             for j in range(self.size[1]):
-                self.grid[-1].append(None)
+                self.grid_visible[-1].append(False)
+                rect = _RectItem(QtCore.QRectF(i, j, 1, 1))
+                self.plot_graph.addItem(rect)
+                self.grid_objects[-1].append(rect)
+                rect.setVisible(False)
 
     def select_cell(self, pos: tuple[int, int]):
         self._validate_pos(pos)
-        if self.grid[pos[0]][pos[1]] is not None:
+        if self.grid_visible[pos[0]][pos[1]]:
             # remove existing square
             logger.warning(f"Cell {pos} already selected")
-        
-        rect = _RectItem(QtCore.QRectF(pos[0], pos[1], 1, 1))
-        self.grid[pos[0]][pos[1]] = rect
-        self.plot_graph.addItem(rect)
+
+        self.grid_objects[pos[0]][pos[1]].setVisible(True)
+        self.grid_visible[pos[0]][pos[1]] = True
 
     def unselect_cell(self, pos: tuple[int, int]):
         self._validate_pos(pos)
-        rect = self.grid[pos[0]][pos[1]]
-        self.plot_graph.removeItem(rect)
-        self.grid[pos[0]][pos[1]] = None
+        self.grid_objects[pos[0]][pos[1]].setVisible(False)
+        self.grid_visible[pos[0]][pos[1]] = False
 
     def _validate_pos(self, pos: tuple[int, int]):
         if len(pos) != 2:
@@ -96,7 +163,7 @@ class GameGridPlot:
             raise ValueError
         if pos[0] >= self.size[0] or pos[1] >= self.size[1]:
             raise ValueError
-        
+
     def _mouse_clicked(self, x):
         if self.size is None:
             return
@@ -114,18 +181,19 @@ class GameGridPlot:
 
 
 class GameGrid:
-    def __init__(self, clickedFnc, size: tuple[int, int] = (10, 10)) -> None:
+    def __init__(self, clickedFnc, stopFnc, size: tuple[int, int] = (10, 10)) -> None:
         self.grid: np.ndarray = None
         self.plot: GameGridPlot = None
         self.size = size
         self._clickedFnc = clickedFnc
+        self._stopFnc = stopFnc
 
         self.set_size(size)
 
     def init_plot(self, widget: QWidget):
         self.plot = GameGridPlot(widget, self._clickedFnc)
         self.set_size(self.size)
-    
+
     def set_size(self, size: tuple[int, int]):
         """ Change size of game grid """
         self.grid = np.zeros(size)
@@ -162,6 +230,9 @@ class GameGrid:
                 else:
                     raise ValueError
 
+        if len(new_alive) == 0 and len(new_dead) == 0:
+            self._stopFnc()
+
         for pos in new_dead:
             self.plot.unselect_cell(pos)
             self.grid[pos[0]][pos[1]] = 0
@@ -173,23 +244,23 @@ class GameGrid:
         x, y = pos
         n_alive = 0
         if x > 0:
-            n_alive += self.grid[x-1][y]
+            n_alive += self.grid[x - 1][y]
         if x < self.grid.shape[0] - 1:
-            n_alive += self.grid[x+1][y]
+            n_alive += self.grid[x + 1][y]
         if y > 0:
-            n_alive += self.grid[x][y-1]
+            n_alive += self.grid[x][y - 1]
         if y < self.grid.shape[1] - 1:
-            n_alive += self.grid[x][y+1]
+            n_alive += self.grid[x][y + 1]
 
         if y > 0 and x > 0:
-            n_alive += self.grid[x-1][y-1]
+            n_alive += self.grid[x - 1][y - 1]
         if x < self.grid.shape[0] - 1 and y < self.grid.shape[1] - 1:
-            n_alive += self.grid[x+1][y+1]
+            n_alive += self.grid[x + 1][y + 1]
 
         if y > 0 and x < self.grid.shape[0] - 1:
-            n_alive += self.grid[x+1][y-1]
+            n_alive += self.grid[x + 1][y - 1]
         if x > 0 and y < self.grid.shape[1] - 1:
-            n_alive += self.grid[x-1][y+1]
+            n_alive += self.grid[x - 1][y + 1]
 
         return n_alive
 
@@ -217,14 +288,30 @@ class _Worker(QObject):
                 self.tick.emit()
 
 
-class WidgetController(QWidget):
-    def __init__(self, parent=None) -> None:
-        super(WidgetController, self).__init__(parent=parent)
-        widget_path = pl.Path(__file__).parent / 'game_of_life.ui'
-        uic.loadUi(widget_path, self)
-        self.parent = parent
-        self.game_grid = GameGrid(self._grid_clicked)
-        self.game_grid.init_plot(self.widget_main)
+class _HelpDialog(QDialog, _help_dialog):
+
+    def __init__(self, parent=None):
+        QDialog.__init__(self, parent, Qt.WindowSystemMenuHint | Qt.WindowTitleHint | Qt.WindowCloseButtonHint)
+
+        self.setupUi(self)
+
+        self.setWindowTitle("Game of Life - Help")
+
+        self.pushButton_wiki.clicked.connect(self._open_wiki)
+
+    def _open_wiki(self):
+        url = QtCore.QUrl("https://en.wikipedia.org/wiki/Conway's_Game_of_Life")
+        if not QtGui.QDesktopServices.openUrl(url):
+            logger.error("Cannot open wiki page")
+
+
+class MainModule:
+    def __init__(self, widget, ds, callback, plot_data, params, settings) -> None:
+        self.widget = widget
+        self.game_grid = GameGrid(self._grid_clicked, self._start_stop_clicked)
+        self.game_grid.init_plot(self.widget.widget_main)
+
+        self._set_icons()
 
         self._connect()
 
@@ -242,24 +329,36 @@ class WidgetController(QWidget):
     def _connect(self, connect: bool = True):
         self._connect_spins()
         self._connect_speed_buttons()
-        self.pushButton_next.clicked.connect(self.game_grid.step_next)
-        self.pushButton_reset.clicked.connect(self.game_grid.reset)
-        self.pushButton_start_stop.clicked.connect(self._start_stop_clicked)
-        self.comboBox.currentTextChanged.connect(self._combo_changed)
+        self._connect_combo()
+        self.widget.pushButton_next.clicked.connect(self.game_grid.step_next)
+        self.widget.pushButton_reset.clicked.connect(self.game_grid.reset)
+        self.widget.pushButton_start_stop.clicked.connect(self._start_stop_clicked)
+        self.widget.pushButton_preset.clicked.connect(self._load_preset)
+        self.widget.pushButton_help.clicked.connect(self._show_help)
+        self.widget.pushButton_save.clicked.connect(self._save_preset)
+
+    def _connect_combo(self, connect=True):
+        if connect:
+            self.widget.comboBox.currentTextChanged.connect(self._combo_changed)
+        else:
+            self.widget.comboBox.disconnect()
 
     def _connect_spins(self, connect: bool = True):
         if connect:
-            self.spinBox_columns.valueChanged.connect(self._spins_changed)
-            self.spinBox_rows.valueChanged.connect(self._spins_changed)
+            self.widget.spinBox_columns.valueChanged.connect(self._spins_changed)
+            self.widget.spinBox_rows.valueChanged.connect(self._spins_changed)
         else:
-            self.spinBox_rows.disconnect()
-            self.spinBox_columns.disconnect()
+            self.widget.spinBox_rows.disconnect()
+            self.widget.spinBox_columns.disconnect()
 
     def _connect_speed_buttons(self, connect: bool = True):
         if connect:
-            self.doubleSpinBox.valueChanged.connect(self._speed_changed)
+            self.widget.doubleSpinBox.valueChanged.connect(self._speed_changed)
         else:
-            self.doubleSpinBox.disconnect()
+            self.widget.doubleSpinBox.disconnect()
+
+    def _set_icons(self):
+        ...
 
     def _grid_clicked(self, pos: tuple[int, int]):
         cell_state = self.game_grid.cell_state(pos)
@@ -283,21 +382,74 @@ class WidgetController(QWidget):
             raise ValueError
 
         self._connect_spins(False)
-        self.spinBox_columns.setValue(x)
-        self.spinBox_rows.setValue(y)
-        self._connect_spins(True)
+        self.widget.spinBox_columns.setValue(x)
+        self.widget.spinBox_rows.setValue(y)
+        self._connect_spins()
         self._spins_changed()
 
     def _spins_changed(self, _=None):
-        x = self.spinBox_columns.value()
-        y = self.spinBox_rows.value()
+        if _ is not None:
+            self._connect_combo(False)
+            self.widget.comboBox.setCurrentText(_grid_custom)
+            self._connect_combo()
+        x = self.widget.spinBox_columns.value()
+        y = self.widget.spinBox_rows.value()
         self.game_grid.set_size((x, y))
 
-    def _start_stop_clicked(self):
-        self.worker.running = not self.worker.running
+    def _start_stop_clicked(self, stop=False, start=False):
+        if stop:
+            self.worker.running = False
+            text = 'Start'
+        elif start:
+            self.worker.running = True
+            text = 'Stop'
+        else:
+            self.worker.running = not self.worker.running
+            if self.worker.running:
+                text = 'Stop'
+            else:
+                text = 'Start'
+        self.widget.pushButton_start_stop.setText(text)
 
     def _speed_changed(self, val: float):
         self.worker.turn_time = val
+
+    def _show_help(self):
+        help_dialog = _HelpDialog(parent=self.widget)
+        help_dialog.exec_()
+
+    def _load_preset(self):
+        options = presets_list()
+        preset_name = select_option(options=options, parent=self.widget, title='Select Preset',
+                                    button_desc='Select preset to be loaded',
+                                    ok_text='Load', action2=remove_preset, action2_text='Remove',
+                                    action2_remove=True)
+        if preset_name is None:
+            return
+
+        self._start_stop_clicked(stop=True)
+
+        preset = load_preset(preset_name)
+
+        # change grid size
+        self._connect_spins(False)
+        self.widget.spinBox_columns.setValue(preset.shape[0])
+        self.widget.spinBox_rows.setValue(preset.shape[1])
+        self._connect_spins(True)
+        self._spins_changed(3)
+
+        for i in range(preset.shape[0]):
+            for j in range(preset.shape[1]):
+                if preset[i][j] == 1:
+                    self.game_grid.select_cell((i, j))
+
+    def _save_preset(self):
+        name = text_input(check_fnc=check_preset_name, parent=self.widget, title='Save Preset', desc='Select name for current preset', ok_text='Save')
+        if name is not None:
+            save_preset(preset_name=name, preset=self.game_grid.grid)
+
+    def update(self, callback_type, origin, active, **optional):
+        ...
 
 
 class Main(QMainWindow):
